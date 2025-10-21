8e08b8f1f7f72f4eae9ee8fe98f55f237c0213db
Joseph Ndayizeye <josephndayizeye250@gmail.com>
2025-10-21 12:02:20 +0200

    added alx book store sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -29,9 +29,10 @@ CREATE TABLE Orders (
 );
 
 CREATE TABLE Order_Details (
-    order_detail_id PRIMARY KEY,
-    order_id FOREIGN KEY REFERENCES Orders.order_id,
-    book_id FOREIGN KEY REFERENCES Books.book_id,
+    order_detail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id, book_id) REFERENCES (Orders(order_id), Books(book_id))
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (BOOK_id) REFERENCES Books(book_id)
 );
