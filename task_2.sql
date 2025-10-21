34fcd0f635a18346e84bd932c7525b7d75b7517b
Joseph Ndayizeye <josephndayizeye250@gmail.com>
2025-10-21 12:30:34 +0200

    Tables in alx book store

---
+++ task_2.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Books (
+    book_id PRIMARY KEY,
+    title VARCHAR(130),
+    author_id FOREIGN KEY REFERENCES Authors.author_id,
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Authors (
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+CREATE TABLE Customers (
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+    order_detail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
